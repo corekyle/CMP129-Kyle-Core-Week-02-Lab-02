5ef0133bf7a2b9d1c006c04d8b193419755863ea
corekyle <core.kyle@student.ccm.edu>
2026-09-15 16:52:46 -0400

    Week 02 Lab 02 Update 1

--- Person.java
+++ Person.java
@@ -1,3 +1,51 @@
-public class Person {
+public class Person 
+{   // START OF Person class
     
-}
+    // Declare fields
+    private String name;
+    private int age;
+    private String email;
+
+
+    // Person setter methods
+    public void setName(String name)
+    {   // START OF setName method
+        name = name;
+    }   // END OF setName method
+
+    public void setAge(int age)
+    {   // START of setAge method
+        age = age;
+    }   // END OF setAge method
+
+    public void setEmail(String email)
+    {   // START OF setEmail method
+        email = email;
+    }   // END OF setEmail method
+    
+
+    // Person getter methods
+    public String getName()
+    {   // START OF getName method
+        return name;
+    }   // END OF getname method
+
+    public int getAge()
+    {   // START OF getAge method
+        return age;
+    }   // END OF getAge method
+
+    public String getEmail()
+    {   // START OF getEmail method
+        return email;
+    }   // END OF getEmail method
+
+
+    // Method to display the details of a person
+    public void displayInfo(String name, int age, String email)
+    {   // START OF displayInfo method
+        System.out.printf("Name: %s\n" +
+                          "Age: %d\n" +
+                          "Email: %s", name, age, email);
+    }   // END OF displayInfo method
+}   // END OF Person class
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,60 @@
-public class PersonTest {
+import java.util.Scanner;
+
+public class PersonTest 
+{   // START OF PersonTest class
     
-}
+    public static void main(String[] args)
+    {   // START OF main method
+
+        // Create Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+
+        // Create Person objects
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+
+        // Declare variables
+        String name;
+        int age;
+        String email;
+
+
+        // Get user input for person 1 info
+        System.out.print("Enter the name of person 1: ");
+        name = keyboard.nextLine();
+        person1.setName(name);
+
+        System.out.print("Enter the age of person 1: ");
+        age = keyboard.nextInt();
+        person1.setAge(age);
+
+        keyboard.nextLine();
+
+        System.out.print("Enter the email address for person 1: ");
+        email = keyboard.nextLine();
+        person1.setEmail(email);
+
+
+        // Get user input for person 2 info
+        System.out.print("Enter the name of person 2: ");
+        name = keyboard.nextLine();
+        person2.setName(name);
+
+        System.out.print("Enter the age of person 2: ");
+        age = keyboard.nextInt();
+        person2.setAge(age);
+
+        keyboard.nextLine();
+
+        System.out.print("Enter the email address for person 2: ");
+        email = keyboard.nextLine();
+        person2.setEmail(email);
+
+        // Display the info about the Person objects
+
+
+    }   // START OF main method
+
+}   // END OF Person Test class
